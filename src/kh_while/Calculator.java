323b3dd6c12663ab5625ffc5a58676d75da3d812
KIM SAYONG <78632508+youss95@users.noreply.github.com>
2021-03-18 16:45:53 +0900

    간단한 계산기에 카운트추가
    한 번 실행 할때마다 count 1증가 count가3이 될시 자동 종료

--- src/kh_while/Calculator.java
+++ src/kh_while/Calculator.java
@@ -5,6 +5,7 @@ public class Quiz_03 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double result =0;
+                 int count=1;
 		while(true) {
 			
 			System.out.println("=== 계산기 프로그램 ====");
@@ -32,13 +33,17 @@ public class Quiz_03 {
 			
 				
 			
-			System.out.printf("%.2f %s %.2f = %.2f\n",n1,op,n2,result);
-			System.out.println("종료 하시겠습니까(q) 이어가기(n) ");
+			System.out.printf("%.2f %s %.2f = %.2f\n 카운트:%d\n",n1,op,n2,result,count);
+			System.out.println("종료 하시겠습니까(q) 이어가기(n) (3번 실행시 종료) ");
+			if(count==3){
+				System.out.println("3번실행되었습니다.");
+				break;}
 			String end = sc.nextLine();
 			if(end.contentEquals("q")) {
 				break;
 			}
 			else if(end.contentEquals("n")) {
+				count++;
 				continue;
 			}
 		}
